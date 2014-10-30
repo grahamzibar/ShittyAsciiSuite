e668217046f6a3fa8c5b26073fd43af74be8ee8b
Graham Robertson <root@graham-robertson.ca>
2014-10-30 19:45:41 -0400

    Working!! (without saving)

--- www/app.js
+++ www/app.js
@@ -1,34 +1,261 @@
-function Grid(_container) {
-	var BLIP_HEIGHT = 10;
-	var BLIP_WIDTH = 7;
+function Blip(_row, _column) {
+	var _value = Blip.EMPTY;
 	
-	var _container_width;
-	var _container_height;
+	this.set_value = function(val) {
+		_value = val;
+	};
+	
+	this.get_value = function() {
+		return _value;
+	};
+	
+	this.clear = function() {
+		_value = Blip.EMPTY;
+	};
+	
+	this.get_coordinates = function() {
+		return {
+			'row': _row,
+			'col': _column
+		};
+	};
+	
+	this.toString = function() {
+		var o = '[';
+		o += _row;
+		o += ', ';
+		o += _column
+		o += '] ';
+		o += _value;
+		return o;
+	};
+};
+Blip.EMPTY = -1;
+
+function Grid(_width, _height) {
+	var _blips = Grid.new_grid(_width, _height);
 	
 	function __constructor__() {
-		var w = _container_width = _container.offsetWidth;
-		var h = _container_height = _container.offsetHeight;
-		
-		var h_amnt = w / BLIP_WIDTH + 1;
-		var v_amnt = h / BLIP_HEIGHT + 1;
-		
-		for (var i = 0, len = Math.round(h_amnt * v_amnt); i < len; i++) {
-			_container.appendChild(create_blip());
+		for (var i = 0, r = _blips, l = _width; i < l; i++) {
+			var c = r[i] = new Array();
+			for (var j = 0, m = _height; j < m; j++)
+				c[j] = new Blip(i, j);
 		}
 	};
 	
-	function create_blip() {
-		var span = document.createElement('span');
-		span.className = 'Blip';
-		span.style.width = BLIP_WIDTH + 'px';
-		span.style.height = BLIP_HEIGHT + 'px';
-		
-		// span.innerHTML = '&heart;';
-		
-		return span;
+	this.get_blips = function() {
+		return _blips;
+	};
+	
+	this.get_blip = function(row, col) {
+		return _blips[row][col];
+	};
+	
+	__constructor__();
+};
+Grid.new_grid = function(rows_num, cols_num) {
+	var rows = new Array(rows_num);
+	
+	for (var i = 0, len = rows.length; i < len; i++)
+		rows[i] = new Array(cols_num);
+	
+	return rows;
+};
+
+function Entity(_num) {
+	this.code = _num;
+	this.toString = function(n) {
+		return n;
+	}.bind(this, _num > -1 ? '&#' + _num + ';' : 'DEL');
+};
+
+function Toolbar() {
+	var RANGES = [
+		[33, 126],
+		[128, 128],
+		[131, 131],
+		[133, 140],
+		[145, 156],
+		[161, 172],
+		[175, 887],
+		[890, 894],
+		[900, 906],
+		[910, 929],
+		[931, 1200],
+		[1201, 1319],
+		[1329, 1366],
+		[1377, 1415],
+		[1488, 1514],
+		[1520, 1524],
+		[1984, 2023],
+		[4053, 4056],
+		[5024, 5108],
+		[5121, 5226],
+		[5572, 5595],
+		[5761, 5782],
+		[8251, 8286],
+		[8414, 8416],
+		[8544, 8582],
+		[8585, 8585],
+		[8592, 8724],
+		[8853, 8879],
+		[8900, 8985],
+		[9096, 9191],
+		// **
+		[9548, 9747],
+		[9750, 9798],
+		[9812, 9854],
+		[9856, 9874],
+		[9876, 9884],
+		[9985, 9988],
+		[9996, 10059],
+		[10063, 10066],
+		[10070, 10070],
+		[10197, 10239],
+		[10496, 10546],
+		[10999, 11000],
+		[11026, 11033],
+		[11568, 11621],
+		[19904, 19967],
+		[63488, 63500],
+		[63549, 63549],
+		[63553, 63563],
+		[119040, 119048],
+		[119056, 119057],
+		[119239, 119246],
+		[119296, 119353],
+		[119552, 119638],
+		[119648, 119665],
+		[120120, 120171],
+		[120488, 120831]
+	];
+	this.tools = new Array();
+	
+	function __constructor__() {
+		this.tools[0] = new Entity(-1);
+		for (var i = 0, r = RANGES, l = r.length; i < l; i++) {
+			for (var j = r[i][0], m = r[i][1] + 1; j < m; j++) {
+				var entity = new Entity(j);
+				this.tools[this.tools.length] = entity;
+			}
+		}
+	};
+	
+	this.toString = function() {
+		var o = '';
+		for (var i = 0, t = this.tools, l = t.length; i < l; i++) {
+			o += t[i].code;
+			o += ': ';
+			o += t[i];
+			o += '\n';
+		}
+		return o;
+	}.bind(this);
+	
+	__constructor__.call(this);
+};
+
+function ToolbarController(_view, _model) {
+	var _selected = null;
+	
+	function __constructor__() {
+		var entities = _model.tools;
+		for (var i = 0, l = entities.length; i < l; i++) {
+			var btn = document.createElement('button');
+			var e = entities[i];
+			btn.title = 'code: ' + e.code;
+			btn.setAttribute('rel', e.code);
+			btn.innerHTML = e;
+			_view.appendChild(btn);
+			
+			if (i === 0) {
+				_selected = btn;
+				btn.disabled = true;
+			}
+		}
+		_view.addEventListener('click', onclick, false);
+	};
+	
+	function onclick(e) {
+		_selected.disabled = false;
+		var t = _selected = e.target;
+		t.disabled = true;
+	};
+	
+	this.get_selected_code = function() {
+		return Number(_selected.getAttribute('rel'));
 	};
 	
 	__constructor__.call(this);
 };
 
-var grid = new Grid(document.getElementById('grid'));
+function GridController(_view, _export_btn, _model, _toolbar_controller) {
+	var BLIP_HEIGHT = 15;
+	var BLIP_WIDTH = 16;
+	
+	function __constructor__() {
+		var w = BLIP_WIDTH;
+		var h = BLIP_HEIGHT;
+		var rows = _model.get_blips();
+		
+		_view.style.width = ((w + 1) * rows[0].length) + 'px';
+		_view.style.height = ((h + 1) * rows.length) + 'px';
+		
+		for (var i = 0, l = rows.length; i < l; i++) {
+			for (var cols = rows[i], j = 0, s = cols.length; j < s; j++) {
+				var a = document.createElement('div');
+				a.className = 'Blip';
+				a.style.width = w + 'px';
+				a.style.height = h + 'px';
+				a.setAttribute('rel', i + '_' + j);
+				
+				_view.appendChild(a);
+			}
+		}
+		
+		_view.addEventListener('click', onclick, false);
+		_export_btn.addEventListener('click', onexport, false);
+	};
+	
+	function onclick(e) {
+		var code = _toolbar_controller.get_selected_code();
+		var coord = e.target.getAttribute('rel').split('_');
+		
+		var blip = _model.get_blip(coord[0], coord[1]);
+		if (code == -1) {
+			blip.clear();
+			e.target.innerHTML = '';
+		} else {
+			blip.set_value(code); // Here, should I store entities?
+			e.target.innerHTML = '&#' + code + ';';
+		}
+	};
+	
+	function onexport(e) {
+		
+	};
+	
+	__constructor__();
+};
+
+/*
+
+	THE APPLICATION
+
+*/
+// SETTINGS
+var WIDTH = 32;
+var HEIGHT = 32;
+
+// MODELS
+var grid = new Grid(WIDTH, HEIGHT);
+var toolbar = new Toolbar();
+
+// CONTROLLERS
+var toolbar_controller = new ToolbarController(document.getElementById('toolbar'), toolbar);
+var grid_controller = new GridController(
+	document.getElementById('grid'),
+	document.getElementById('export_btn'),
+	grid,
+	toolbar_controller
+);
